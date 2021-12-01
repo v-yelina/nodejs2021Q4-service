c0c723f77a294cd7b1a93ec3039c3a7604699054
v-yelina <heytridvaodin@gmail.com>
2021-12-01 13:37:57 +0100

    feat: create server using fastify

--- src/server.js
+++ src/server.js
@@ -1,6 +1,45 @@
-const { PORT } = require('./common/config');
-const app = require('./app');
+const app = require('fastify')({ logger: true });
+const fastifyEnv = require('fastify-env');
 
-app.listen(PORT, () =>
-  console.log(`App is running on http://localhost:${PORT}`)
-);
+const schema = {
+  type: 'object',
+  required: ['PORT'],
+  properties: {
+    PORT: {
+      type: 'integer',
+      default: 3000,
+    },
+  },
+};
+
+const options = {
+  confKey: 'config',
+  schema,
+  dotenv: true,
+  data: process.env,
+};
+
+app.register(fastifyEnv, options).ready((err) => {
+  if (err) console.error(err);
+});
+
+app.get('/', async (request, reply) => {
+  reply.send({ hello: 'This is Task-4 REST service' });
+});
+
+// const userRoutes = require("./src/routes/user.route");
+// userRoutes.forEach((route, index) => {
+//   app.route(route);
+// });
+
+const start = async () => {
+  try {
+    await app.ready();
+    await app.listen(app.config.PORT);
+    app.log.info(`Server listening on PORTs:${app.config.PORT}`);
+  } catch (err) {
+    app.log.error(err);
+    process.exit(1);
+  }
+};
+start();
