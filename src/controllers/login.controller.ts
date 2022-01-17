a9dd3a3b68c325ac504d9d04cd6bb8c7e9c4d39d
v-yelina <heytridvaodin@gmail.com>
2022-01-17 14:37:45 +0100

    refactor: move login controller to separate file

---
+++ src/controllers/login.controller.ts
@@ -0,0 +1,48 @@
+import { FastifyReply, FastifyRequest } from 'fastify';
+import { getRepository } from 'typeorm';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import { IUserLogin } from '../interfaces/user.interfaces';
+import { EUser } from '../entity/user.entity';
+import ENV from 'src/common/config';
+
+export async function login(
+  request: FastifyRequest<{ Body: IUserLogin }>,
+  reply: FastifyReply
+): Promise<{ name: string; id: string; login: string } | undefined> {
+  const login: string = request.body.login;
+  const user: EUser | undefined = await getRepository(EUser).findOne({
+    where: { login },
+  });
+  if (!user) {
+    return reply
+      .status(401)
+      .send({ token: null, message: 'User not found or Incorrect password' });
+  }
+
+  const passwordIsValid = bcrypt.compareSync(
+    request.body.password,
+    user.password as string
+  );
+
+  if (!passwordIsValid) {
+    return reply
+      .status(401)
+      .send({ token: null, message: 'User not found or Incorrect password' });
+  }
+
+  const token = jwt.sign(
+    { id: user.id, login: user.login },
+    ENV.JWT_SECRET_KEY as string,
+    {
+      expiresIn: 86400, // in sec => 24 hours
+    }
+  );
+
+  reply.send({
+    id: user.id,
+    name: user.name,
+    login: user.login,
+    accessToken: token,
+  });
+}
--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,16 +1,9 @@
 import { v4 as uuid } from 'uuid';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { getRepository } from 'typeorm';
-import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
-import {
-  IUser,
-  INewUser,
-  IUserUpdate,
-  IUserLogin,
-} from '../interfaces/user.interfaces';
+import { IUser, INewUser, IUserUpdate } from '../interfaces/user.interfaces';
 import { EUser } from '../entity/user.entity';
-import ENV from 'src/common/config';
 
 /**
  * Returns all users
@@ -123,45 +116,4 @@ export async function deleteUser(
   return delUser.affected
     ? reply.status(204).send()
     : reply.status(404).send('User with such ID was not found');
-}
-
-export async function login(
-  request: FastifyRequest<{ Body: IUserLogin }>,
-  reply: FastifyReply
-): Promise<{ name: string; id: string; login: string } | undefined> {
-  const login: string = request.body.login;
-  const user: EUser | undefined = await getRepository(EUser).findOne({
-    where: { login },
-  });
-  if (!user) {
-    return reply
-      .status(401)
-      .send({ token: null, message: 'User not found or Incorrect password' });
-  }
-
-  const passwordIsValid = bcrypt.compareSync(
-    request.body.password,
-    user.password as string
-  );
-
-  if (!passwordIsValid) {
-    return reply
-      .status(401)
-      .send({ token: null, message: 'User not found or Incorrect password' });
-  }
-
-  const token = jwt.sign(
-    { id: user.id, login: user.login },
-    ENV.JWT_SECRET_KEY as string,
-    {
-      expiresIn: 86400, // in sec => 24 hours
-    }
-  );
-
-  reply.send({
-    id: user.id,
-    name: user.name,
-    login: user.login,
-    accessToken: token,
-  });
 }
