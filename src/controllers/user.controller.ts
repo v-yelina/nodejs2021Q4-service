06a0a4d31b6b444eba60470bbdf748c68ab00117
v-yelina <heytridvaodin@gmail.com>
2022-01-17 14:34:21 +0100

    feat: add login controller

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,8 +1,16 @@
 import { v4 as uuid } from 'uuid';
 import { FastifyReply, FastifyRequest } from 'fastify';
 import { getRepository } from 'typeorm';
-import { IUser, INewUser, IUserUpdate } from '../interfaces/user.interfaces';
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
+import {
+  IUser,
+  INewUser,
+  IUserUpdate,
+  IUserLogin,
+} from '../interfaces/user.interfaces';
 import { EUser } from '../entity/user.entity';
+import ENV from 'src/common/config';
 
 /**
  * Returns all users
@@ -54,7 +62,10 @@ export async function addUser(
   request: FastifyRequest<{ Body: IUserUpdate }>,
   reply: FastifyReply
 ): Promise<{ name: string; id: string; login: string } | undefined> {
-  const data = request.body;
+  const data = {
+    ...request.body,
+    password: bcrypt.hashSync(request.body.password, 8),
+  };
   const newUser: INewUser | undefined = { id: uuid(), ...data };
   await getRepository(EUser).insert([newUser]);
   return reply
@@ -112,4 +123,45 @@ export async function deleteUser(
   return delUser.affected
     ? reply.status(204).send()
     : reply.status(404).send('User with such ID was not found');
+}
+
+export async function login(
+  request: FastifyRequest<{ Body: IUserLogin }>,
+  reply: FastifyReply
+): Promise<{ name: string; id: string; login: string } | undefined> {
+  const login: string = request.body.login;
+  const user: EUser | undefined = await getRepository(EUser).findOne({
+    where: { login },
+  });
+  if (!user) {
+    return reply
+      .status(401)
+      .send({ token: null, message: 'User not found or Incorrect password' });
+  }
+
+  const passwordIsValid = bcrypt.compareSync(
+    request.body.password,
+    user.password as string
+  );
+
+  if (!passwordIsValid) {
+    return reply
+      .status(401)
+      .send({ token: null, message: 'User not found or Incorrect password' });
+  }
+
+  const token = jwt.sign(
+    { id: user.id, login: user.login },
+    ENV.JWT_SECRET_KEY as string,
+    {
+      expiresIn: 86400, // in sec => 24 hours
+    }
+  );
+
+  reply.send({
+    id: user.id,
+    name: user.name,
+    login: user.login,
+    accessToken: token,
+  });
 }
